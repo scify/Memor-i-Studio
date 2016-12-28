8129a727f560c721142b9a14d749fa6c8949c58b
Pavlos Isaris <paulisaris@gmail.com>
2016-12-28 16:24:00 +0200

    Added views for login, signup, reset and general elements MGOR-24

--- resources/views/auth/forms/resetPasswordEmail.blade.php
+++ resources/views/auth/forms/resetPasswordEmail.blade.php
@@ -11,31 +11,34 @@
 
         <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
             {{ csrf_field() }}
+            <div class="form-content">
+                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
-            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                <div class="col-md-6">
-                    <div class="inputer">
-                        <div class="input-wrapper">
-                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                    @if ($errors->has('email'))
-                        <span class="help-block">
+                    <div class="col-md-6">
+                        <div class="inputer">
+                            <div class="input-wrapper">
+                                <input id="email" type="email" class="form-control" name="email"
+                                       value="{{ old('email') }}" required>
+                            </div>
+                            @if ($errors->has('email'))
+                                <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                    @endif
+                            @endif
                         </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-md-6 col-md-offset-4">
+                        <button type="submit" class="btn btn-primary">
+                            Send Password Reset Link
+                        </button>
+                    </div>
                 </div>
             </div>
 
-            <div class="form-group">
-                <div class="col-md-6 col-md-offset-4">
-                    <button type="submit" class="btn btn-primary">
-                        Send Password Reset Link
-                    </button>
-                </div>
-            </div>
         </form>
     </div>
 </div>
