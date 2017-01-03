d39fc1d883500ba5fe785af4f5a84df16450e782
Pavlos Isaris <paulisaris@gmail.com>
2017-01-03 15:07:59 +0200

    Added controller responsible for internal app resources (images, sounds eg)

---
+++ app/Http/Controllers/DataController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
+
+class DataController extends Controller
+{
+    public function resolvePath($dir, $subDir, $filename) {
+        $path = storage_path() . '/app/' . $dir . '/' . $subDir . '/' . $filename;
+        $response = null;
+
+
+        if(File::exists($path)) {
+
+            $file = File::get($path);
+            $type = File::mimeType($path);
+            $response = Response::make($file, 200);
+            $response->header("Content-Type", $type);
+        }
+
+        return $response;
+    }
+}
