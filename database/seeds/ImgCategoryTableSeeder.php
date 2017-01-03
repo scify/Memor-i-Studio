1daf3b1f8cfbdc758b84d740bccfe8095706a20d
Pavlos Isaris <paulisaris@gmail.com>
2017-01-03 15:06:04 +0200

    Added migrations and seeds for tables

--- database/seeds/ImgCategoryTableSeeder.php
+++ database/seeds/ImgCategoryTableSeeder.php
@@ -14,6 +14,7 @@ class ImgCategoryTableSeeder extends Seeder
         DB::table('image_category')->delete();
         //insert some dummy records
         DB::table('image_category')->insert(array(
+            array('category'=>'game_cover','description'=>'The cover image for the game'),
             array('category'=>'card_images','description'=>'Each card is associated with 1 or 2 images')
         ));
     }
