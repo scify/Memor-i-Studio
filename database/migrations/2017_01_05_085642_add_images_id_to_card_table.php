eee35dc128da77857f42b5b40eb17d627b48144c
Pavlos Isaris <paulisaris@gmail.com>
2017-01-05 11:03:03 +0200

    Added migration for card images

---
+++ database/migrations/2017_01_05_085642_add_images_id_to_card_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddImagesIdToCardTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('card', function ($table) {
+            $table->integer('image_id')->unsigned();
+            $table->integer('negative_image_id')->unsigned()->nullable();
+        });
+
+        Schema::table('card', function ($table) {
+            $table->foreign('image_id')->references('id')->on('image');
+            $table->foreign('negative_image_id')->references('id')->on('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('card', function(Blueprint $table){
+            $table->dropForeign('card_image_id_foreign');
+            $table->dropColumn('image_id');
+            $table->dropForeign('card_negative_image_id_foreign');
+            $table->dropColumn('negative_image_id');
+        });
+    }
+}
