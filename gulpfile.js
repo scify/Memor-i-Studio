202da465e6bd68504e8790d77299a51abba96800
Pavlos Isaris <paulisaris@gmail.com>
2017-01-10 14:50:27 +0200

    Updated laravel elixir processes to include all js files into one master file

--- gulpfile.js
+++ gulpfile.js
@@ -20,49 +20,57 @@ elixir(function(mix) {
         'public/css/style.css'
     ], 'public/dist/app.css', './');
 
-    var directories = {
-        'node_modules/npm-modernizr': 'public/dist/npm-modernizr',
-        'node_modules/chosen-js': 'public/dist/chosen-js',
-        'public/assets/pleasure-admin-panel/js': 'public/dist/pleasure-admin-panel/js',
-        'node_modules/bxslider': 'public/dist/bxslider',
-        'node_modules/jquery-knob': 'public/dist/jquery-knob',
-        'node_modules/html2canvas': 'public/dist/html2canvas',
-        'node_modules/gauge-js': 'public/dist/gauge-js',
-        'node_modules/jasny-bootstrap': 'public/dist/jasny-bootstrap',
-        'node_modules/sweetalert': 'public/dist/sweetalert',
-        'node_modules/datatables': 'public/dist/datatables',
-        'node_modules/icheck': 'public/dist/icheck',
-        'public/assets/pleasure-admin-panel/fonts': 'public/build/fonts'
+    // var directories = {
+    //     'node_modules/npm-modernizr': 'public/dist/npm-modernizr',
+    //     'node_modules/chosen-js': 'public/dist/chosen-js',
+    //     'public/assets/pleasure-admin-panel/js': 'public/dist/pleasure-admin-panel/js',
+    //     'node_modules/bxslider': 'public/dist/bxslider',
+    //     'node_modules/jquery-knob': 'public/dist/jquery-knob',
+    //     'node_modules/html2canvas': 'public/dist/html2canvas',
+    //     'node_modules/gauge-js': 'public/dist/gauge-js',
+    //     'node_modules/jasny-bootstrap': 'public/dist/jasny-bootstrap',
+    //     'node_modules/sweetalert': 'public/dist/sweetalert',
+    //     'node_modules/datatables': 'public/dist/datatables',
+    //     'node_modules/icheck': 'public/dist/icheck',
+    //     'public/assets/pleasure-admin-panel/fonts': 'public/build/fonts'
+    //
+    // };
+    //
+    // for (var directory in directories) {
+    //     mix.copy(directory, directories[directory]);
+    // }
 
-    };
-
-    for (var directory in directories) {
-        mix.copy(directory, directories[directory]);
-    }
-
-    // mix.scripts([
-    //     'node_modules/npm-modernizr/modernizr.js',
-    //     'node_modules/jquery/dist/jquery.min.js',
-    //     'public/assets/pleasure-admin-panel/js/global-vendors.js',
-    //     'node_modules/bootstrap/dist/js/bootstrap.min.js',
-    //     'node_modules/bxslider/dist/jquery.bxslider.min.js',
-    //     'node_modules/jquery-knob/dist/jquery.knob.min.js',
-    //     'node_modules/jquery-knob/excanvas.js',
-    //     'node_modules/html2canvas/dist/html2canvas.min.js',
-    //     'node_modules/html2canvas/dist/html2canvas.svg.min.js',
-    //     'node_modules/gauge-js/dest/gauge.min.js',
-    //     'node_modules/jasny-bootstrap/dist/js/jasny-bootstrap.min.js',
-    //     'node_modules/sweetalert/dist/sweetalert.min.js',
-    //     'node_modules/datatables/media/js/jquery.dataTables.min.js',
-    //     'node_modules/icheck/icheck.min.js',
-    //     'public/assets/pleasure-admin-panel/js/sliders.js',
-    //     'public/assets/pleasure-admin-panel/js/layout.js',
-    //     'public/assets/pleasure-admin-panel/js/pleasure.js'
-    // ], 'public/dist/app.js' , './');
+    mix.scripts([
+        'node_modules/npm-modernizr/modernizr.js',
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/jquery-ui-dist/jquery-ui.min.js',
+        'node_modules/chosen-js/chosen.jquery.js',
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'node_modules/velocity-animate/velocity.min.js',
+        'node_modules/moment/moment.js',
+        'node_modules/toastr/toastr.js',
+        'node_modules/scrollmonitor/scrollMonitor.js',
+        'node_modules/textarea-autosize/dist/jquery.textarea_autosize.min.js',
+        'node_modules/bootstrap-select/dist/js/bootstrap-select.min.js',
+        'node_modules/fastclick/lib/fastclick.js',
+        'node_modules/bxslider/dist/jquery.bxslider.min.js',
+        'node_modules/jquery-knob/dist/jquery.knob.min.js',
+        'node_modules/jquery-knob/excanvas.js',
+        'node_modules/html2canvas/dist/html2canvas.min.js',
+        'node_modules/html2canvas/dist/html2canvas.svg.min.js',
+        'node_modules/gauge-js/dest/gauge.min.js',
+        'node_modules/jasny-bootstrap/dist/js/jasny-bootstrap.min.js',
+        'node_modules/sweetalert/dist/sweetalert.min.js',
+        'node_modules/datatables/media/js/jquery.dataTables.min.js',
+        'node_modules/icheck/icheck.min.js',
+        'public/assets/pleasure-admin-panel/js/sliders.js',
+        'public/assets/pleasure-admin-panel/js/layout.js',
+        'public/assets/pleasure-admin-panel/js/pleasure.js'
+    ], 'public/dist/app.js' , './');
 
 
     //the parameter is relative to the public directory
-    mix.version(['dist/app.css']);
+    mix.version(['dist/app.css', 'dist/app.js']);
 
     // mix.sass('app.scss',
     //     'public/assets/css/app.css')
--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,23 +1,23 @@
-<script src="{{asset("dist/pleasure-admin-panel/js/global-vendors.js")}}"></script>
-<script src="{{asset("dist/npm-modernizr/modernizr.js")}}"></script>
-<script src="{{asset("dist/chosen-js/chosen.jquery.js")}}"></script>
+{{--<script src="{{asset("dist/pleasure-admin-panel/js/global-vendors.js")}}"></script>--}}
+{{--<script src="{{asset("dist/npm-modernizr/modernizr.js")}}"></script>--}}
+{{--<script src="{{asset("dist/chosen-js/chosen.jquery.js")}}"></script>--}}
 
-<script src="{{asset("dist/bxslider/dist/jquery.bxslider.min.js")}}"></script>
-<script src="{{asset("dist/jquery-knob/dist/jquery.knob.min.js")}}"></script>
-<script src="{{asset("dist/jquery-knob/excanvas.js")}}"></script>
-<script src="{{asset("dist/html2canvas/dist/html2canvas.min.js")}}"></script>
-<script src="{{asset("dist/html2canvas/dist/html2canvas.svg.min.js")}}"></script>
-<script src="{{asset("dist/gauge-js/dest/gauge.min.js")}}"></script>
-<script src="{{asset("dist/jasny-bootstrap/dist/js/jasny-bootstrap.min.js")}}"></script>
-<script src="{{asset("dist/sweetalert/dist/sweetalert.min.js")}}"></script>
-<script src="{{asset("dist/datatables/media/js/jquery.dataTables.min.js")}}"></script>
-<script src="{{asset("dist/icheck/icheck.min.js")}}"></script>
+{{--<script src="{{asset("dist/bxslider/dist/jquery.bxslider.min.js")}}"></script>--}}
+{{--<script src="{{asset("dist/jquery-knob/dist/jquery.knob.min.js")}}"></script>--}}
+{{--<script src="{{asset("dist/jquery-knob/excanvas.js")}}"></script>--}}
+{{--<script src="{{asset("dist/html2canvas/dist/html2canvas.min.js")}}"></script>--}}
+{{--<script src="{{asset("dist/html2canvas/dist/html2canvas.svg.min.js")}}"></script>--}}
+{{--<script src="{{asset("dist/gauge-js/dest/gauge.min.js")}}"></script>--}}
+{{--<script src="{{asset("dist/jasny-bootstrap/dist/js/jasny-bootstrap.min.js")}}"></script>--}}
+{{--<script src="{{asset("dist/sweetalert/dist/sweetalert.min.js")}}"></script>--}}
+{{--<script src="{{asset("dist/datatables/media/js/jquery.dataTables.min.js")}}"></script>--}}
+{{--<script src="{{asset("dist/icheck/icheck.min.js")}}"></script>--}}
 
-<script src="{{asset("dist/pleasure-admin-panel/js/sliders.js")}}"></script>
-<script src="{{asset("dist/pleasure-admin-panel/js/layout.js")}}"></script>
-<script src="{{asset("dist/pleasure-admin-panel/js/pleasure.js")}}"></script>
-{{--<script src="{{asset(elixir('dist/app.js'))}}">--}}
-{{--</script>--}}
+{{--<script src="{{asset("dist/pleasure-admin-panel/js/sliders.js")}}"></script>--}}
+{{--<script src="{{asset("dist/pleasure-admin-panel/js/layout.js")}}"></script>--}}
+{{--<script src="{{asset("dist/pleasure-admin-panel/js/pleasure.js")}}"></script>--}}
+<script src="{{asset(elixir('dist/app.js'))}}">
+</script>
 
 <script>
     $(document).ready(function () {
