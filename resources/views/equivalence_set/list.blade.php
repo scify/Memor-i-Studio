4158cea321d5db3adeac61e66d03f77d6669ea48
Pavlos Isaris <paulisaris@gmail.com>
2017-01-11 15:53:07 +0200

    Functionality to display appropriate message after game flavor creation, redirect to cards page. Functionality to control view of an unpublished game flavor

--- resources/views/equivalence_set/list.blade.php
+++ resources/views/equivalence_set/list.blade.php
@@ -1,13 +1,25 @@
 @extends('common.layout')
 @section('content')
+    <div>
     <ol class="breadcrumb">
         <li class="breadcrumb-item"><a href="{{url('home')}}"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
         <li class="breadcrumb-item active">{{$gameFlavor->name}}</li>
     </ol>
-    @if(count($equivalenceSets) == 0)
-        <div class="alert alert-info">
-            <strong>This game flavor does not contain any card sets!</strong> @if($gameFlavor->accessed_by_user)Press the "+" button to add one.@endif
-        </div>
+    </div>
+    @if($gameFlavor->accessed_by_user)
+        @if(count($equivalenceSets) == 0)
+            <div class="alert alert-warning noFloatAlert">
+                <strong>This game flavor does not contain any card sets!</strong> Press the "+" button to add one.
+            </div>
+        @elseif(count($equivalenceSets) < 2)
+            <div class="alert alert-info noFloatAlert">
+                <strong>This game flavor contains only one card set!</strong> In order to proceed, add at least one card set more.
+            </div>
+        @else
+            <div class="alert alert-info noFloatAlert">
+                Click <a href="">here</a> to proceed to game sounds
+            </div>
+        @endif
     @endif
     <div class="row">
         <div class="col-md-12">
