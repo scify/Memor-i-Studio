6e9f1ebea82e7ad33446b6e8d1bef40c3c1334dd
Pavlos Isaris <paulisaris@gmail.com>
2017-01-12 14:06:56 +0200

    Aesthetical improvements on cards view list

--- resources/views/equivalence_set/list.blade.php
+++ resources/views/equivalence_set/list.blade.php
@@ -24,7 +24,7 @@
     <div class="row">
         <div class="col-md-12">
             @if($gameFlavor->accessed_by_user)
-                <div class="col-md-5">
+                <div class="col-md-6">
                     <button id="newEquivalenceSetBtn" class="btn btn-default btn-ripple" data-toggle="modal"
                             data-target="#cardOptions">
                         <i class="fa fa-plus" aria-hidden="true"></i>
--- resources/views/equivalence_set/single.blade.php
+++ resources/views/equivalence_set/single.blade.php
@@ -1,11 +1,17 @@
-<div class="col-md-5 equivalenceSet">
-    @if($gameFlavor->accessed_by_user)
-        <div class="margin-bottom-10">
-            <button class="btn btn-primary btn-ripple deleteSetBtn" data-equivalenceSetId="{{$equivalenceSet->id}}">
-                <i class="fa fa-trash" aria-hidden="true"></i>
-            </button>
+<div class="col-md-6">
+    <div class="equivalenceSetContainer">
+    <div class="equivalenceSet">
+        @if($gameFlavor->accessed_by_user)
+            <div class="margin-bottom-10">
+                <button class="btn btn-primary btn-ripple deleteSetBtn" data-equivalenceSetId="{{$equivalenceSet->id}}">
+                    <i class="fa fa-trash" aria-hidden="true"></i>
+                </button>
+            </div>
+        @endif
+        <div class="cardList">
+            @include('card.list', ['cards' => $equivalenceSet->cards()->get()])
         </div>
-    @endif
-    @include('card.list', ['cards' => $equivalenceSet->cards()->get()])
+    </div>
+    </div>
 </div>
 
