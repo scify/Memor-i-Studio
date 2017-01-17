aa8db62d85e47fc9ca6294585d610ff40ee87e6a
Pavlos Isaris <paulisaris@gmail.com>
2017-01-17 09:57:35 +0200

    functionality to parse and create Resource Categories instances

--- app/BusinessLogicLayer/managers/ResourceCategoryManager.php
+++ app/BusinessLogicLayer/managers/ResourceCategoryManager.php
@@ -1,11 +1,57 @@
 <?php
+namespace App\BusinessLogicLayer\managers;
+
+use App\Models\GameVersion;
+use App\Models\ResourceCategory;
+use App\StorageLayer\ResourceCategoryStorage;
 
 /**
- * Created by IntelliJ IDEA.
- * User: pisaris
- * Date: 16/1/2017
- * Time: 2:25 μμ
+ * Class containing the Business logic for Resource Categories
  */
 class ResourceCategoryManager {
+
+    private $resourceCategoryStorage;
+
+    public function __construct() {
+        $this->resourceCategoryStorage = new ResourceCategoryStorage();
+    }
+
+    /**
+     * Given an array of resources paths, parse each path as a new @see ResourceCategory
+     *
+     * @param array $resources the array containing the resourcesPath
+     * @param $gameVersionId int the id of the GameVersion these ResourceCategory instances will belong to
+     */
+    public function createResourceCategoriesFromResourcesArray(array $resources, $gameVersionId) {
+        foreach ($resources as $resource) {
+            $this->createNewResourceCategory($resource, $gameVersionId);
+        }
+    }
+
+    /**
+     * Given a category path and a @see GameVersion id, create a new @see ResourceCategory instance
+     *
+     * @param $categoryPath string the application path of the resource category
+     * @param $gameVersionId int the id of the GameVersion these ResourceCategory instance will belong to
+     * @return mixed|null the newly crated ResourceCategory instance, or null if an error occurred.
+     */
+    public function createNewResourceCategory($categoryPath, $gameVersionId) {
+        $resourceCategory = new ResourceCategory();
+        $resourceCategory->path = $categoryPath;
+        $resourceCategory->game_version_id = $gameVersionId;
+        $resourceCategory->description = trim($categoryPath, "/");
+        return $this->resourceCategoryStorage->storeResourceCategory($resourceCategory);
+    }
+
+    /**
+     * Given a Resource category name, get the instance corresponding to it.
+     *
+     * @param $resourceCategoryName string the resource category name
+     * @param $gameVersionId int the game version id of the resource category
+     * @return mixed the ResourceCategory instance, or null if no occurrences found.
+     */
+    public function getResourceCategoryByNameForGameVersion($resourceCategoryName, $gameVersionId) {
+        return $this->resourceCategoryStorage->getResourceCategoryByPathForGameVersion($resourceCategoryName, $gameVersionId);
+    }
 
 }
