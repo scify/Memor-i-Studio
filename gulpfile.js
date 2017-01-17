f939feeec6bad60d261617551aa0e04e4d9daf5a
Pavlos Isaris <paulisaris@gmail.com>
2017-01-17 16:08:04 +0200

    Commented out unused npm libraries

--- gulpfile.js
+++ gulpfile.js
@@ -19,10 +19,11 @@ elixir(function(mix) {
         'resources/assets/pleasure-admin-panel/css/elements.css',
         'node_modules/sweetalert/dist/sweetalert.css',
 
-        'node_modules/chosen-js/chosen.css',
-        'node_modules/bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.min.css',
-        'node_modules/jasny-bootstrap/dist/css/jasny-bootstrap.min.css',
-        'node_modules/bxslider/dist/jquery.bxslider.min.css',
+        // 'node_modules/chosen-js/chosen.css',
+        // 'node_modules/bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.min.css',
+        // 'node_modules/jasny-bootstrap/dist/css/jasny-bootstrap.min.css',
+        // 'node_modules/bxslider/dist/jquery.bxslider.min.css',
+        'node_modules/bootstrap-select/dist/css/bootstrap-select.min.css',
         'node_modules/datatables/media/css/jquery.dataTables.min.css',
         'node_modules/icheck/skins/square/blue.css'
     ], 'public/css/vendors.css', './');
@@ -49,19 +50,19 @@ elixir(function(mix) {
         'node_modules/textarea-autosize/dist/jquery.textarea_autosize.min.js',
         'node_modules/bootstrap-select/dist/js/bootstrap-select.min.js',
         'node_modules/fastclick/lib/fastclick.js',
-        'node_modules/bxslider/dist/jquery.bxslider.min.js',
-        'node_modules/jquery-knob/dist/jquery.knob.min.js',
-        'node_modules/jquery-knob/excanvas.js',
-        'node_modules/html2canvas/dist/html2canvas.min.js',
-        'node_modules/html2canvas/dist/html2canvas.svg.min.js',
-        'node_modules/jasny-bootstrap/dist/js/jasny-bootstrap.min.js',
+        // 'node_modules/bxslider/dist/jquery.bxslider.min.js',
+        // 'node_modules/jquery-knob/dist/jquery.knob.min.js',
+        // 'node_modules/jquery-knob/excanvas.js',
+        // 'node_modules/html2canvas/dist/html2canvas.min.js',
+        // 'node_modules/html2canvas/dist/html2canvas.svg.min.js',
+        // 'node_modules/jasny-bootstrap/dist/js/jasny-bootstrap.min.js',
         'node_modules/sweetalert/dist/sweetalert.min.js',
         'node_modules/datatables/media/js/jquery.dataTables.min.js',
-        'node_modules/icheck/icheck.min.js',
+        // 'node_modules/icheck/icheck.min.js',
         'resources/assets/pleasure-admin-panel/js/sliders.js',
         'resources/assets/pleasure-admin-panel/js/layout.js',
         'resources/assets/pleasure-admin-panel/js/pleasure.js'
-    ],
+        ],
         'public/js/app.js' , './')
         .scripts([
             'resources/assets/js/controllers/'
