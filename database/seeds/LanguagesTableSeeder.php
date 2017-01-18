4835eb471ebac628e45081d6b2fd2fb32d17d148
Pavlos Isaris <paulisaris@gmail.com>
2017-01-18 11:11:06 +0200

    Updated laravel seeder to include more languages

--- database/seeds/LanguagesTableSeeder.php
+++ database/seeds/LanguagesTableSeeder.php
@@ -16,6 +16,10 @@ class LanguagesTableSeeder extends Seeder
         DB::table('language')->insert(array(
             array('name'=>'Greek','code'=>'el','flag_img_path'=>'el.png'),
             array('name'=>'English','code'=>'en','flag_img_path'=>'en.png'),
+            array('name'=>'German','code'=>'de','flag_img_path'=>'de.png'),
+            array('name'=>'French','code'=>'fr','flag_img_path'=>'fr.png'),
+            array('name'=>'Spanish','code'=>'es','flag_img_path'=>'es.png'),
+            array('name'=>'Norwegian','code'=>'no','flag_img_path'=>'no.png'),
         ));
     }
 }
