2c4d86031149dbbb04ce537c759c3d413dd06f42
Pavlos Isaris <paulisaris@gmail.com>
2017-01-18 17:03:12 +0200

    Updated views for game version resources

--- resources/views/game_resource_category/list.blade.php
+++ resources/views/game_resource_category/list.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
-            <div class="panel-group accordion">
+            <div class="panel-group accordion padding-left-30">
                 @foreach($resourceCategories as $index => $resourceCategory)
                     @include('game_resource.list', ['resources' => $resourceCategory->resources, 'index' => $index])
                 @endforeach
