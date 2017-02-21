396d8dcbb61a2fc60d41ee491aab39fead1c36f0
Pavlos Isaris <paulisaris@gmail.com>
2017-02-21 12:12:03 +0200

    Added class responsible for file management

---
+++ app/BusinessLogicLayer/managers/FileManager.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\BusinessLogicLayer\managers;
+
+use Illuminate\Support\Facades\File;
+
+class FileManager {
+
+    public function copyFileToDestinationAndReplace($sourceFile, $destinationFile) {
+
+        if(File::exists($destinationFile)) {
+            File::delete($destinationFile);
+        }
+        if ( ! File::copy($sourceFile, $destinationFile)) {
+            throw new \Exception("Couldn't copy file " . $sourceFile . " to " . $destinationFile);
+        }
+    }
+
+    public function replaceStringInFileWith($filePath, $stringToBeReplaced, $newString) {
+        $command = "sed -i 's/" . $stringToBeReplaced . "/" . $newString . "/g' " . $filePath;
+        $output = shell_exec($command);
+        return $output;
+    }
+
+}
