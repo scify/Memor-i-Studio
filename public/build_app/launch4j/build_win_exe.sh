7630875c2fab283680370ec60035e848c6f0fb10
Pavlos Isaris <paulisaris@gmail.com>
2017-03-07 14:40:29 +0200

    Refactored file that calls the launch4j executable

--- public/build_app/launch4j/build_win_exe.sh
+++ public/build_app/launch4j/build_win_exe.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
+#launch4j_application/launch4j.sh $1
 
-./launch4j_application/launch4j $1
+#!/bin/sh
+LAUNCH4J="$(dirname "$0")"/launch4j_application/launch4j.jar
+echo LAUNCH4J: "$LAUNCH4J"
+if [ -n "$JAVA_HOME" ]; then
+  $JAVA_HOME/bin/java -jar "$LAUNCH4J" "$@"
+else
+  java -jar "$LAUNCH4J" "$@"
+fi
