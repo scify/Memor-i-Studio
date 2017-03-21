bb0890b1b3d662551ed7d55bd83ae32a730d4c29
Pavlos Isaris <paulisaris@gmail.com>
2017-03-21 15:59:05 +0200

    Finalized home page

--- resources/views/common/home.blade.php
+++ resources/views/common/home.blade.php
@@ -12,20 +12,46 @@
             </a>
         </div><!--.col-md-6-->
     </div>
-        <div class="row">
+    <div class="row">
         <div class="col-md-12 centeredVertically text-align-center padding-15">
             <div class="card card-video card-player-blue">
 
                 <div class="card-heading padding-top-20">
-                    <h4>How to create a game?</h4>
-                    <p>Think of a game story, create cards and customize your game!</p>
+                    <h4>Καλώς ήρθες στο Memor-i Studio! </h4>
+                </div><!--.card-heading-->
+
+                <div class="card-body">
+                    {{--<iframe width="600" height="400"--}}
+                            {{--src="https://www.youtube.com/embed/We2iD21V9WI">--}}
+                    {{--</iframe>--}}
+                    <ul class="list">
+                        <li>Βρες δωρεάν παιχνίδια Memor-i</li>
+                        <li>Σχεδίασε το δικό σου παιχνίδι Memor-i, χωρίς γνώσεις προγραμματισμού!</li>
+                        <li>Διασκέδαση και μάθηση για τυφλούς αλλά και βλέποντες</li>
+                    </ul>
+                </div><!--.card-body-->
+
+            </div><!--.card-->
+        </div><!--.col-md-6-->
+    </div>
+    <div class="row">
+        <div class="col-md-12 centeredVertically text-align-center padding-15">
+            <div class="card card-video card-player-blue">
+
+                <div class="card-heading padding-top-20">
+                    <h4>Welcome to Memor-i Studio!</h4>
 
                 </div><!--.card-heading-->
 
                 <div class="card-body">
-                    <iframe width="600" height="400"
-                            src="https://www.youtube.com/embed/We2iD21V9WI">
-                    </iframe>
+                    {{--<iframe width="600" height="400"--}}
+                            {{--src="https://www.youtube.com/embed/We2iD21V9WI">--}}
+                    {{--</iframe>--}}
+                    <ul class="list">
+                        <li>Find free Memor-i games</li>
+                        <li>Create your own Memor-i game without programming knowledge!</li>
+                        <li>Learn and have fun whether you’re blind or not</li>
+                    </ul>
                 </div><!--.card-body-->
 
             </div><!--.card-->
