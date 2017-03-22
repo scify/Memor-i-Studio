1a9e3f1916d29c7a0a32fe82c309eea4269b460b
Pavlos Isaris <paulisaris@gmail.com>
2017-03-22 12:17:21 +0200

    Added Notifiable trait to User model so that they can receive reset password emails

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
-use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
-    use Authenticatable, CanResetPassword;
+    use Authenticatable, CanResetPassword, Notifiable;
 
     /**
      * The table associated with the model.
