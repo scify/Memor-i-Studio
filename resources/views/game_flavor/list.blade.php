36d89f8cd4bab08a38314e2abf0f354206ec4ff7
Pavlos Isaris <paulisaris@gmail.com>
2017-03-24 16:44:53 +0200

    Aesthetical improvements

--- resources/views/game_flavor/list.blade.php
+++ resources/views/game_flavor/list.blade.php
@@ -3,7 +3,7 @@
     <div class="row">
         <div class="col-md-12">
         @foreach($gameFlavors as $gameFlavor)
-                <div class="col-md-3">
+                <div class="col-md-4">
                     @include('game_flavor.single', ['gameFlavor' => $gameFlavor, 'user' => \Illuminate\Support\Facades\Auth::user()])
                 </div>
         @endforeach
