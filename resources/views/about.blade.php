494d02a1f6c188e23fabfd468cc2b8f72932900b
Pavlos Isaris <paulisaris@gmail.com>
2017-03-24 12:54:10 +0200

    MGOR-125 Additional info in about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -12,6 +12,48 @@
                     The Memor-i studio project has been funded by <a href="http://www.latsis-foundation.org/eng">Public Benefit Foundation John S. Latsis.</a>
 
                     For more information, click <a href="http://www.scify.gr/site/en/impact-areas-en/assistive-technologies/memorien">here.</a>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row aboutPageRow">
+        <div class="col-md-10 centeredVertically">
+            <div class="panel">
+                <div class="panel-heading">
+                    <div class="panel-title"><h4>DETAILS</h4></div>
+                </div><!--.panel-heading-->
+                <div class="panel-body padding-30">
+                    <h4>Πληροφορίες Memor-i Studio</h4>
+                    <b>Τι είναι</b>
+                    <br>
+                    Το <i>Memor-i Studio</i> είναι μια πλατφόρμα που επιτρέπει τη δημιουργία νέων ηλεκτρονικών παιχνιδιών από τον καθένα χωρίς γνώσεις προγραμματισμού και πληροφορικής. Μπορείτε να:
+                    <br>
+                    <ul class="list margin-bottom-20 margin-top-20">
+                        <li><i class="fa fa-check" aria-hidden="true"></i> κατεβάσετε παιχνίδια δωρεάν</li>
+                        <li><i class="fa fa-check" aria-hidden="true"></i> δημιουργήσετε νέα παιχνίδια Memor-i με καινούριους ήχους και εικόνες.</li>
+                        <li><i class="fa fa-check" aria-hidden="true"></i> να δημιουργήσετε παραλλαγές (‘κλώνους’) ενός παιχνιδιού και να το εμπλουτίσετε με επιπλέον εικόνες και ήχους.</li>
+                    </ul>
+                    <b>Σε ποιους απευθύνεται</b>
+                    <br>
+                    Τα παιχνίδια που μπορείτε να δημιουργήσετε μέσω της πλατφόρμας απευθύνονται σε ανθρώπους με τυφλότητα (ηχητικό αρχείο), σε ανθρώπους με προβλήματα όρασης (ασπρόμαυρη εικόνα) καθώς και σε βλέποντες (έγχρωμη εικόνα).
+                    <br>
+                    <br>
+                    <br>
+                    <h4>Information about Memor-i Studio</h4>
+                    <b>What is Memor-i Studio?</b>
+                    <br>
+                    <i>Memor-i Studio</i> is a platform that allows anyone without programming skills to create new inclusive electronic games. You can:
+                    <br>
+                    <ul class="list margin-bottom-20 margin-top-20">
+                        <li><i class="fa fa-check" aria-hidden="true"></i> download games for free</li>
+                        <li><i class="fa fa-check" aria-hidden="true"></i> create new Memor-i games with new sounds and images</li>
+                        <li><i class="fa fa-check" aria-hidden="true"></i> create variants ('clones') of a game and enrich it with additional images and sounds.</li>
+                    </ul>
+                    <b>Who can use Memor-i Studio</b>
+                    <br>
+                    The games are created through the platform are targeted at people with blindness (audio file), people with visual impairments (black and white image) and sighted (color image).
+
                 </div>
             </div>
         </div>
