ceede021811e370134c2cf1d4347ba906a30f69e
Pavlos Isaris <paulisaris@gmail.com>
2017-03-24 12:59:36 +0200

    MGOR-126 Help menu items in horizontal menu

--- resources/views/common/header/navbarHorizontal.blade.php
+++ resources/views/common/header/navbarHorizontal.blade.php
@@ -7,6 +7,14 @@
             <li class="{{ (Route::current()->getName() == 'showAllGameFlavors') ? 'active' : '' }}"><a href="{{route('showAllGameFlavors')}}"><i class="fa fa-gamepad" aria-hidden="true"></i> All Games</a></li>
             <li class="{{ (Route::current()->getName() == 'showAboutPage') ? 'active' : '' }}"><a href="{{route('showAboutPage')}}">About</a></li>
             <li class="{{ (Route::current()->getName() == 'showContactForm') ? 'active' : '' }}"><a href="{{route('showContactForm')}}">Contact</a></li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown">Help
+                    <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="https://docs.google.com/document/d/1irOmObgn9ZwEqmwn3uXNvQTJmgt1YiMqFx5Tzp-l_Oo/edit#">English</a></li>
+                    <li><a href="https://docs.google.com/document/d/1whAITueSBuaX9Gd0VY85X0cv5CWQ6CwkjbZULoBxugs/edit#">Greek</a></li>
+                </ul>
+            </li>
         </ul>
         <div class="pull-right">
             <ul class="nav navbar-nav">
