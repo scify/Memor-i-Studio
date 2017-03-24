1757965b71859a0c62b0e05a4ed9475a12d85270
Pavlos Isaris <paulisaris@gmail.com>
2017-03-24 14:23:23 +0200

    Refactored home page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,27 +1,34 @@
 @extends('layouts.app')
 @section('content')
-    <div class="row">
-        <div class="col-md-6 centeredVertically">
+
+    <div class="row aboutPageRow">
+        <div class="col-md-6">
             <div class="panel">
                 <div class="panel-heading">
-                    <div class="panel-title"><h4>ABOUT</h4></div>
+                    <div class="panel-title"><h4>Details</h4></div>
                 </div><!--.panel-heading-->
                 <div class="panel-body padding-30">
-                    Memor-i studio has been created by <a href="http://www.scify.gr/site/en/">Science For You (SciFY)</a>, a Greek not-for-profit organization.
-
-                    The Memor-i studio project has been funded by <a href="http://www.latsis-foundation.org/eng">Public Benefit Foundation John S. Latsis.</a>
-
-                    For more information, click <a href="http://www.scify.gr/site/en/impact-areas-en/assistive-technologies/memorien">here.</a>
-
+                    <h4>Information about Memor-i Studio</h4>
+                    <b>What is Memor-i Studio?</b>
+                    <br>
+                    <i>Memor-i Studio</i> is a platform that allows anyone without programming skills to create new inclusive electronic games. You can:
+                    <br>
+                    <ul class="list margin-bottom-20 margin-top-20">
+                        <li><i class="fa fa-check" aria-hidden="true"></i> download games for free</li>
+                        <li><i class="fa fa-check" aria-hidden="true"></i> create new Memor-i games with new sounds and images</li>
+                        <li><i class="fa fa-check" aria-hidden="true"></i> create variants ('clones') of a game and enrich it with additional images and sounds.</li>
+                    </ul>
+                    <b>Who can use Memor-i Studio</b>
+                    <br>
+                    The games are created through the platform are targeted at people with blindness (audio file), people with visual impairments (black and white image) and sighted (color image).
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row aboutPageRow">
-        <div class="col-md-10 centeredVertically">
+
+        <div class="col-md-6">
             <div class="panel">
                 <div class="panel-heading">
-                    <div class="panel-title"><h4>DETAILS</h4></div>
+                    <div class="panel-title"><h4>Πληροφορίες</h4></div>
                 </div><!--.panel-heading-->
                 <div class="panel-body padding-30">
                     <h4>Πληροφορίες Memor-i Studio</h4>
@@ -37,22 +44,23 @@
                     <b>Σε ποιους απευθύνεται</b>
                     <br>
                     Τα παιχνίδια που μπορείτε να δημιουργήσετε μέσω της πλατφόρμας απευθύνονται σε ανθρώπους με τυφλότητα (ηχητικό αρχείο), σε ανθρώπους με προβλήματα όρασης (ασπρόμαυρη εικόνα) καθώς και σε βλέποντες (έγχρωμη εικόνα).
-                    <br>
-                    <br>
-                    <br>
-                    <h4>Information about Memor-i Studio</h4>
-                    <b>What is Memor-i Studio?</b>
-                    <br>
-                    <i>Memor-i Studio</i> is a platform that allows anyone without programming skills to create new inclusive electronic games. You can:
-                    <br>
-                    <ul class="list margin-bottom-20 margin-top-20">
-                        <li><i class="fa fa-check" aria-hidden="true"></i> download games for free</li>
-                        <li><i class="fa fa-check" aria-hidden="true"></i> create new Memor-i games with new sounds and images</li>
-                        <li><i class="fa fa-check" aria-hidden="true"></i> create variants ('clones') of a game and enrich it with additional images and sounds.</li>
-                    </ul>
-                    <b>Who can use Memor-i Studio</b>
-                    <br>
-                    The games are created through the platform are targeted at people with blindness (audio file), people with visual impairments (black and white image) and sighted (color image).
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6 centeredVertically">
+            <div class="panel">
+                <div class="panel-heading">
+                    <div class="panel-title"><h4>ABOUT</h4></div>
+                </div><!--.panel-heading-->
+                <div class="panel-body padding-30">
+                    Memor-i studio has been created by <a href="http://www.scify.gr/site/en/">Science For You (SciFY)</a>, a Greek not-for-profit organization.
+
+                    The Memor-i studio project has been funded by <a href="http://www.latsis-foundation.org/eng">Public Benefit Foundation John S. Latsis.</a>
+
+                    For more information, click <a href="http://www.scify.gr/site/en/impact-areas-en/assistive-technologies/memorien">here.</a>
 
                 </div>
             </div>
--- resources/views/common/home.blade.php
+++ resources/views/common/home.blade.php
@@ -1,38 +1,20 @@
 @extends('layouts.app')
 @section('content')
     <div class="row margin-top-50 margin-bottom-50 memoriActionBtns">
-        <div class="col-md-6 text-align-center">
-            <a href="{{route('showGameVersionSelectionForm')}}" class="btn btn-success btn-ripple ">
+        <div class="col-md-6">
+        <div class="col-md-6 text-align-center centeredVertically">
+            <a href="{{route('showGameVersionSelectionForm')}}" class="btn btn-success btn-ripple width-percent-100">
                 <h1>Create new Game</h1>
             </a>
         </div><!--.col-md-6-->
-        <div class="col-md-6 text-align-center">
-            <a href="{{route('showAllGameFlavors')}}" class="btn btn-primary btn-ripple ">
+        </div>
+        <div class="col-md-6">
+        <div class="col-md-6 text-align-center centeredVertically">
+            <a href="{{route('showAllGameFlavors')}}" class="btn btn-primary btn-ripple width-percent-100">
                 <h1>See all Games</h1>
             </a>
         </div><!--.col-md-6-->
-    </div>
-    <div class="row homePageRow">
-        <div class="col-md-12 centeredVertically text-align-center padding-15">
-            <div class="card card-video card-player-blue">
-
-                <div class="card-heading padding-top-20">
-                    <h4>Καλώς ήρθες στο Memor-i Studio! </h4>
-                </div><!--.card-heading-->
-
-                <div class="card-body">
-                    {{--<iframe width="600" height="400"--}}
-                            {{--src="https://www.youtube.com/embed/We2iD21V9WI">--}}
-                    {{--</iframe>--}}
-                    <ul class="list">
-                        <li><i class="fa fa-check" aria-hidden="true"></i> Βρες δωρεάν παιχνίδια Memor-i</li>
-                        <li><i class="fa fa-check" aria-hidden="true"></i> Σχεδίασε το δικό σου παιχνίδι Memor-i, χωρίς γνώσεις προγραμματισμού!</li>
-                        <li><i class="fa fa-check" aria-hidden="true"></i> Διασκέδαση και μάθηση για τυφλούς αλλά και βλέποντες</li>
-                    </ul>
-                </div><!--.card-body-->
-
-            </div><!--.card-->
-        </div><!--.col-md-6-->
+        </div>
     </div>
     <div class="row homePageRow">
         <div class="col-md-12 centeredVertically text-align-center padding-15">
@@ -51,6 +33,28 @@
                         <li><i class="fa fa-check" aria-hidden="true"></i> Find free Memor-i games</li>
                         <li><i class="fa fa-check" aria-hidden="true"></i> Create your own Memor-i game without programming knowledge!</li>
                         <li><i class="fa fa-check" aria-hidden="true"></i> Learn and have fun whether you’re blind or not</li>
+                    </ul>
+                </div><!--.card-body-->
+
+            </div><!--.card-->
+        </div><!--.col-md-6-->
+    </div>
+    <div class="row homePageRow">
+        <div class="col-md-12 centeredVertically text-align-center padding-15">
+            <div class="card card-video card-player-blue">
+
+                <div class="card-heading padding-top-20">
+                    <h4>Καλώς ήρθες στο Memor-i Studio! </h4>
+                </div><!--.card-heading-->
+
+                <div class="card-body">
+                    {{--<iframe width="600" height="400"--}}
+                    {{--src="https://www.youtube.com/embed/We2iD21V9WI">--}}
+                    {{--</iframe>--}}
+                    <ul class="list">
+                        <li><i class="fa fa-check" aria-hidden="true"></i> Βρες δωρεάν παιχνίδια Memor-i</li>
+                        <li><i class="fa fa-check" aria-hidden="true"></i> Σχεδίασε το δικό σου παιχνίδι Memor-i, χωρίς γνώσεις προγραμματισμού!</li>
+                        <li><i class="fa fa-check" aria-hidden="true"></i> Διασκέδαση και μάθηση για τυφλούς αλλά και βλέποντες</li>
                     </ul>
                 </div><!--.card-body-->
 
