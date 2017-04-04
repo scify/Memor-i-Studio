3960309c911444968d493f0938582e21d5abadd6
Pavlos Isaris <paulisaris@gmail.com>
2017-04-04 15:55:44 +0300

    Altered email template for game approval e-mail

--- resources/views/email/game_flavor_approved.blade.php
+++ resources/views/email/game_flavor_approved.blade.php
@@ -2,7 +2,13 @@
 @section('title', 'Congratulations!')
 @section('body')
     <p style="text-align: center">
-        Congratulations! Your game: '{{$gameFlavor->name}}' was just published!
-        Click <a href="http://memoristudio.scify.org/games"> here</a> to see and download it.
+        Congratulations! Your game: '{{$gameFlavor->name}}' was just built!
+        <br>
+        Remember, that in order for the game to be publicly available for download,
+        You need to click the "Make public" at the game box area in the homepage.
+        <br>
+        Until you make your game public, only you can download and play it.
+        <br>
+        Click <a href="http://memoristudio.scify.org/games"> here</a> to see, download and make it public!
     </p>
 @stop
