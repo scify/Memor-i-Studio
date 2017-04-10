b5036ac8527f9f4ff1d83fb02763a826b32487c6
Pavlos Isaris <paulisaris@gmail.com>
2017-04-10 10:37:29 +0300

    The email sent to admin after teh game flavor submission contains als othe game flavor id and the user id

--- resources/views/email/game_flavor_submission_admin.blade.php
+++ resources/views/email/game_flavor_submission_admin.blade.php
@@ -2,10 +2,12 @@
 @section('title', 'Game approval')
 @section('body')
     <p style="text-align: center">
-        The creator of the game: '{{$gameFlavor->name}}' just submitted it for approval.
+        The creator of the game: '{{$gameFlavor->name}}' just submitted it for approval (game flavor id: {{$gameFlavor->id}}).
         <br>
         <br>
         Creator details:
+        <br>
+        id: {{$gameFlavor->creator->id}}
         <br>
         email: {{$gameFlavor->creator->email}}
         <br>
