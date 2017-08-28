721618a6ebed3760d4e15a728a7e9e707a868a66
Pavlos Isaris <paulisaris@gmail.com>
2017-08-28 10:39:53 +0300

    Created player table migration

---
+++ database/migrations/2017_08_24_080744_create_player_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('player', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_name');
+            $table->string('app_id', 200)->comment('The unique app id produced from the application');
+            $table->dateTime('last_seen_online')->nullable();
+            $table->boolean('in_game')->default(false)->comment('Whether the player is currently in a game session');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('player');
+    }
+
+}
