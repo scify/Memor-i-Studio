0bcecfa78f87104cba55ae4426e3c5ece08ffed7
Pavlos Isaris <paulisaris@gmail.com>
2017-09-22 12:47:17 +0300

    Updated form of selecting a game version

--- resources/views/game_flavor/forms/select_game_version.blade.php
+++ resources/views/game_flavor/forms/select_game_version.blade.php
@@ -11,15 +11,15 @@
                           action="{{route('createGameFlavorIndex')}}"
                           enctype="multipart/form-data">
                         <div class="row">
-                            <div class="col-md-3">Game Type</div><!--.col-md-3-->
-                            <div class="col-md-9">
+                            <div class="col-md-2">Game Type</div><!--.col-md-3-->
+                            <div class="col-md-8">
                                 <select class="selecter width-percent-100" name="game_version_id">
                                     @foreach($gameVersions as $gameVersion)
                                         <option value="{{$gameVersion->id}}">{{$gameVersion->name}} {{$gameVersion->version_code}}</option>
                                     @endforeach
                                 </select>
                             </div><!--.col-md-9-->
-                            <div class="col-md-3">
+                            <div class="col-md-2">
                                 <button type="submit" id="gameVersionLangSubmitBtn" class="btn btn-primary btn-ripple">
                                     Continue
                                 </button>
