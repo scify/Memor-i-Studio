c50fd559cb300c3af2af7b03124889a692bf3c1d
Paul Isaris <paulisaris@gmail.com>
2018-06-27 11:00:26 +0300

    - Made aesthetic changes for Tablet view

--- resources/views/equivalence_set/list.blade.php
+++ resources/views/equivalence_set/list.blade.php
@@ -2,32 +2,37 @@
 @section('content')
     <div class="">
         <ol class="breadcrumb col-md-6">
-            <li class="breadcrumb-item"><a href="{{url('home')}}"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
+            <li class="breadcrumb-item"><a href="{{url('home')}}"><i class="fa fa-home" aria-hidden="true"></i> Home</a>
+            </li>
             <li class="breadcrumb-item active">{{$gameFlavor->name}}</li>
         </ol>
-        @if($gameFlavor->is_built)
-            <button class="margin-left-20 btn btn-primary btn-ripple" data-toggle="modal" data-target="#downloadLinksModal">Download</button>
-        @endif
     </div>
     @if($gameFlavor->accessed_by_user)
-        @if(count($equivalenceSets) == 0)
-            <div class="alert alert-warning noFloatAlert">
-                <strong>This game flavor does not contain any card sets!</strong> Press the "+" button to add one.
-            </div>
-        @elseif(count($equivalenceSets) < 2)
-            <div class="alert alert-info noFloatAlert">
-                <strong>This game flavor contains only one card set!</strong> In order to proceed, add at least one card set more.
-            </div>
-        @else
-            <div class="row">
-                <div class="col-md-9">
-                    <div class="alert alert-info noFloatAlert">
-                        Click <a href="{{route('getResourcesForGameFlavor', ['id' => $gameFlavor->id])}}">here</a> to proceed to game sounds (optional)
+        <div class="row">
+            @if(count($equivalenceSets) == 0)
+                <div class="col-md-6">
+                    <div class="alert alert-warning noFloatAlert">
+                        <strong>This game flavor does not contain any card sets!</strong> Press the "+" button to add
+                        one.
                     </div>
                 </div>
-                <div class="col-md-3">
+            @elseif(count($equivalenceSets) < 2)
+                <div class="col-md-6">
+                    <div class="alert alert-info noFloatAlert">
+                        <strong>This game flavor contains only one card set!</strong> In order to proceed, add at least
+                        one card set more.
+                    </div>
+                </div>
+            @else
+                <div class="col-md-6">
+                    <div class="alert alert-info noFloatAlert">
+                        Click <a href="{{route('getResourcesForGameFlavor', ['id' => $gameFlavor->id])}}">here</a> to
+                        proceed to game sounds (optional)
+                    </div>
+                </div>
+                <div class="col-md-2 margin-bottom-20">
                     @if($gameFlavor->submitted_for_approval)
-                        <a href="#" class="disabled btn btn-success btn-ripple padding-15">
+                        <a href="#" class="width-percent-100 disabled btn btn-success btn-ripple padding-15">
                             Submitted
                         </a>
                     @else
@@ -38,9 +43,15 @@
                             </button>
                         </form>
                     @endif
-                </div><!--.col-md-9-->
-            </div>
-        @endif
+                </div>
+                <div class="col-md-2 margin-bottom-20">
+                    @if($gameFlavor->is_built)
+                        <a class="width-percent-100 btn btn-primary btn-ripple padding-15" data-toggle="modal"
+                           data-target="#downloadLinksModal">Download</a>
+                    @endif
+                </div>
+        </div>
+    @endif
     @endif
     <div class="row">
         <div class="col-md-12">
@@ -66,12 +77,16 @@
                 <div class="modal-body" style="height: 200px;">
                     <div class="row downloadBtnContainer">
                         <ul class="margin-bottom-30">
-                            <li style="text-align: left">For Windows, run the installer .exe file to install the game.</li>
-                            <li style="text-align: left">For Linux, right click the .jar file -> Open with -> Oracle Java 8</li>
+                            <li style="text-align: left">For Windows, run the installer .exe file to install the game.
+                            </li>
+                            <li style="text-align: left">For Linux, right click the .jar file -> Open with -> Oracle
+                                Java 8
+                            </li>
                         </ul>
                         <div class="col-md-6">
-                            <a data-gameFlavorId="{{$gameFlavor->id}}" class="downloadBtnWindows" id="tooltip-{{$gameFlavor->id}}"
-                               title = "Run the installer .exe file to install the game"
+                            <a data-gameFlavorId="{{$gameFlavor->id}}" class="downloadBtnWindows"
+                               id="tooltip-{{$gameFlavor->id}}"
+                               title="Run the installer .exe file to install the game"
                                href="{{route('downloadGameFlavorWindows', $gameFlavor->id)}}">
                                 <button class="downloadBtn btn btn-primary btn-ripple">
                                     <i class="fa fa-windows" aria-hidden="true"></i> Windows
@@ -79,8 +94,9 @@
                             </a>
                         </div>
                         <div class="col-md-6">
-                            <a data-gameFlavorId="{{$gameFlavor->id}}" class="downloadBtnLinux" id="tooltip-{{$gameFlavor->id}}"
-                               title = "Right click -> Open with -> Oracle Java 8"
+                            <a data-gameFlavorId="{{$gameFlavor->id}}" class="downloadBtnLinux"
+                               id="tooltip-{{$gameFlavor->id}}"
+                               title="Right click -> Open with -> Oracle Java 8"
                                href="{{route('downloadGameFlavorLinux', $gameFlavor->id)}}">
                                 <button class="downloadBtn btn btn-primary btn-ripple">
                                     <i class="fa fa-linux" aria-hidden="true"></i> Linux
@@ -99,7 +115,7 @@
 @endsection
 @section('additionalFooter')
     <script>
-        $(function() {
+        $(function () {
             $("[id^=tooltip-]").tooltip();
             var controller = new window.EquivalenceSetsController(cards, editCardRoute);
             controller.init();
