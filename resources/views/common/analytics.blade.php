50206815fcf877fc31573cf4d0a74515ed0e088b
Paul Isaris <paulisaris@gmail.com>
2022-02-21 12:09:24 +0200

    Double instead of single quotes

--- resources/views/common/analytics.blade.php
+++ resources/views/common/analytics.blade.php
@@ -9,17 +9,19 @@
         // Your web app's Firebase configuration
         // For Firebase JS SDK v7.20.0 and later, measurementId is optional
         const firebaseConfig = {
-            apiKey: '{{ config('app.firebase_api_key') }}',
+            apiKey: "{{ config('app.firebase_api_key') }}",
             authDomain: "memor-i-desktop.firebaseapp.com",
             databaseURL: "https://memor-i-desktop-default-rtdb.europe-west1.firebasedatabase.app",
             projectId: "memor-i-desktop",
             storageBucket: "memor-i-desktop.appspot.com",
-            messagingSenderId: '{{ config('app.firebase_messaging_sender_id') }}',
-            appId: '{{ config('app.firebase_app_id') }}',
-            measurementId: '{{ config('app.firebase_measurement_id') }}'
+            messagingSenderId: "{{ config('app.firebase_messaging_sender_id') }}",
+            appId: "{{ config('app.firebase_app_id') }}",
+            measurementId: "{{ config('app.firebase_measurement_id') }}"
         };
         // Initialize Firebase
         const app = initializeApp(firebaseConfig);
         const analytics = getAnalytics(app);
+        console.log(app);
+        console.log(analytics);
     </script>
 @endif
