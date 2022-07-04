cc10fedb26c8b79edf9452b27f67e70a99d3b247
Paul Isaris <paulisaris@gmail.com>
2022-07-04 10:15:09 +0300

    Error logging

--- app/Http/Controllers/Auth/SHAPES/ShapesIntegrationController.php
+++ app/Http/Controllers/Auth/SHAPES/ShapesIntegrationController.php
@@ -75,7 +75,7 @@ class ShapesIntegrationController extends Controller {
             session()->flash('flash_message_success', trans('messages.welcome_to') . ' Memor-i Studio!');
             return $this->login($request);
         } catch (Exception $e) {
-            Log::error($e->getMessage());
+            Log::error(json_encode($e));
             session()->flash('flash_message_failure', $e->getMessage());
             return redirect()->back();
         }
