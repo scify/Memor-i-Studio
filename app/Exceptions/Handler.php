cb6df1eb578daca53f60637daf3148451934ff8a
Paul Isaris <paulisaris@gmail.com>
2022-09-30 11:09:09 +0300

    Sentry version

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -9,7 +9,6 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Log;
 use Throwable;
 
 class Handler extends ExceptionHandler {
