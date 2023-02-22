e76544c51edb94191a02f816064fb3d572aa3fb0
Paul Isaris <paulisaris@gmail.com>
2023-02-22 14:48:42 +0200

    Added optional parameter

--- public/build_app/innosetup/iscc.sh
+++ public/build_app/innosetup/iscc.sh
@@ -9,6 +9,14 @@ echo "Wine base dir: $WINEPREFIX"
 
 SCRIPTNAME=$2
 INNO_BIN="Inno Setup 5/ISCC.exe"
+# Get inno setup path
+if $# -gt 2; then
+  INNO_PATH=$3
+else
+  INNO_PATH="${PROGFILES_PATH%?}/${INNO_BIN}"
+fi
+# pwd
+echo "Looking inno setup in : $INNO_PATH";
 
 # Check if variable is set
 [ -z "$SCRIPTNAME" ] && { echo "Usage: $0 <USER_WITH_WINE_INSTALLED> <SCRIPT_NAME>"; echo; exit 1; }
@@ -26,12 +34,6 @@ echo "Got program files path as: $PROGRAMFILES"
 # Translate windows path to absolute unix path
 PROGFILES_PATH=$(winepath -u "${PROGRAMFILES}" )
 echo "Program files translated as linux path: $PROGFILES_PATH"
-
-
-# Get inno setup path
-INNO_PATH="${PROGFILES_PATH%?}/${INNO_BIN}"
-# pwd
-echo "Looking inno setup in : $INNO_PATH";
 
 # Translate unix script path to windows path
 SCRIPTNAME=$(winepath -w "$SCRIPTNAME" )
