76faed8726e1455654870fdf50b36d3946ba3610
Paul Isaris <paulisaris@gmail.com>
2023-03-16 15:11:16 +0200

    Fixed routes

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,26 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Console\Commands\BuildGameFlavors;
 use App\Console\Commands\BuildGameFlavorsForVersion;
 use Illuminate\Support\Facades\Artisan;
 
 class AdminController extends Controller {
 
-    public function buildGameFlavors(int $gameFlavorId) {
-        $result = Artisan::call(BuildGameFlavorsForVersion::$COMMAND, [
-            'id' => $gameFlavorId
+    public function buildGameFlavors(int $id) {
+        $result = Artisan::call(BuildGameFlavors::$COMMAND, [
+            'id' => $id
         ]);
         return response()->json([
-            "command" => BuildGameFlavorsForVersion::$COMMAND . ' ' . $gameFlavorId,
+            "command" => BuildGameFlavors::$COMMAND . ' ' . $id,
             "result" => $result
         ]);
     }
 
-    public function buildGameFlavorsForVersion(int $gameVersionId) {
+    public function buildGameFlavorsForVersion(int $id) {
         $result = Artisan::call(BuildGameFlavorsForVersion::$COMMAND, [
-            'id' => $gameVersionId
+            'id' => $id
         ]);
         return response()->json([
+            "command" => BuildGameFlavorsForVersion::$COMMAND . ' ' . $id,
             "result" => $result
         ]);
     }
