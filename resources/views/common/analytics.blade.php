85ab7d4c2a76943c56baf061da124b3dc30cdfd9
Paul Isaris <paulisaris@gmail.com>
2023-04-06 15:52:05 +0300

    Anonymize IP in Google Analytics

--- resources/views/common/analytics.blade.php
+++ resources/views/common/analytics.blade.php
@@ -40,6 +40,7 @@
             })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
             ga('create', '{{ config('app.google_analytics_id') }}', 'auto');
+            ga('set', 'anonymizeIp', true);
             ga('send', 'pageview');
         </script>
     @endif
