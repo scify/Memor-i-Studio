807cb84c6f3b2bf5f8304d0c30750230d0bf411a
Paul Isaris <paulisaris@gmail.com>
2023-08-24 14:15:16 +0300

    Changed command

--- public/build_app/launch4j/build_win_exe.sh
+++ public/build_app/launch4j/build_win_exe.sh
@@ -3,7 +3,7 @@ LAUNCH4J="$(dirname "$0")"/launch4j_application/launch4j.jar
 echo LAUNCH4J: "$LAUNCH4J"
 COMMAND=("java -jar $LAUNCH4J $@")
 if [ -n "$JAVA_HOME" ]; then
-  COMMAND=("$(JAVA_HOME)/bin/java -jar $LAUNCH4J $@")
+  COMMAND=("$JAVA_HOME/bin/java -jar $LAUNCH4J $@")
 fi
 echo "${COMMAND[0]}"
 
