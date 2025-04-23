a4b810643fa404b8544ab2edc8390bf10826f52c
Paul Isaris <paulisaris@gmail.com>
2025-04-23 11:07:38 +0300

    recpatcha config

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -10,7 +10,7 @@
                     <form id="gameVersion-handling-form" class="memoriForm" method="POST"
                           action="{{route('sendContactEmail')}}"
                           enctype="multipart/form-data">
-                        {!! RecaptchaV3::field('contact', ['name' => 'g-recaptcha-response']) !!}
+                        {!! RecaptchaV3::field('contact') !!}
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <div class="row">
                             <div class="inputer floating-label">
