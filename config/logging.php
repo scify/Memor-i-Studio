47ef932023463a155a5b9e5b1355c78fccfc8e69
Paul Isaris <paulisaris@gmail.com>
2025-05-02 08:21:50 +0300

    removed Sentry from simple logging

--- config/logging.php
+++ config/logging.php
@@ -35,7 +35,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['daily', 'sentry'],
+            'channels' => ['daily'],
         ],
 
         'single' => [
